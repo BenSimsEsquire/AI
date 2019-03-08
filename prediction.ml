d746284df9154283e7deaf1cb359201385d279d2
BenSimsEsquire <benbbsims@gmail.com>
2019-03-08 14:52:54 +0000

    First tests for prediction function

--- prediction.ml
+++ prediction.ml
@@ -1,1 +1,3 @@
-let prediction() = 'y';;
+let prediction activation =
+	if activation >= 0.0 then 1.0 else 0.0;;
+
--- test.ml
+++ test.ml
@@ -1,18 +1,18 @@
 open OUnit2;;
 
-let test1 test_ctxt = assert_equal "x" (Foo.unity "x");;
 
-let test2 test_ctxt = assert_equal 100 (Foo.unity 100);;
-
-
-let test3 test_ctxt =  assert_equal 'y' (Prediction.prediction ());;
+let test1 test_ctxt =  assert_equal 1.0 (Prediction.prediction 1.0) ;;
+let test2 test_ctxt =  assert_equal 1.0 (Prediction.prediction 2.0) ;;
+let test3 test_ctxt =  assert_equal 1.0 (Prediction.prediction 0.0) ;;
+let test4 test_ctxt =  assert_equal 0.0 (Prediction.prediction (-1.0)) ;;
 
 (* Name the test cases and group them together *)
 let suite =
 "suite">:::
  ["test1">:: test1;
   "test2">:: test2;
-  "test3">:: test3]
+  "test3">:: test3;
+  "test4">:: test4]
 ;;
 
 let () =
